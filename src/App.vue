979266430c1e2578ef185a50f101f97977f770f2
web17code <web17code@163.com>
2017-08-22 18:19:42 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,126 @@
+<style scoped>
+
+  .layout{
+    border: 0px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
+  }
+  .layout .content{
+    min-height: 100vh;
+  }
+  .layout-breadcrumb{
+    padding: 10px 15px 0;
+  }
+  .layout-content{
+    min-height: 200px;
+    margin: 15px;
+    overflow: hidden;
+    background: #fff;
+    border-radius: 4px;
+  }
+  .layout-content-main{
+    padding: 10px;
+  }
+  .layout-copy{
+    text-align: center;
+    padding: 10px 0 20px;
+    color: #9ea7b4;
+  }
+  .layout-menu-left{
+    background: #464c5b;
+  }
+  .layout-header{
+    height: 60px;
+    background: #fff;
+    box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    text-align: center;
+  }
+  .layout-logo-left{
+    width: 90%;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    margin: 15px auto;
+  }
+  .layout-ceiling-main a{
+    color: #9ba7b5;
+  }
+  .layout-hide-text .layout-text{
+    display: none;
+  }
+  .ivu-col{
+    transition: width .2s ease-in-out;
+  }
+</style>
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div class="layout" :class="{'layout-hide-text': spanLeft < 5}">
+    <Row type="flex" class="content">
+      <i-col :span="spanLeft" class="layout-menu-left">
+        <Menu active-name="1" theme="dark" width="auto">
+          <!--<div class="layout-logo-left"></div>-->
+          <Menu-item name="1">
+            <Icon type="ios-navigate" :size="iconSize"></Icon>
+            <span class="layout-text">选项 1</span>
+          </Menu-item>
+          <Menu-item name="2">
+            <Icon type="ios-keypad" :size="iconSize"></Icon>
+            <span class="layout-text">选项 2</span>
+          </Menu-item>
+          <Menu-item name="3">
+            <Icon type="ios-analytics" :size="iconSize"></Icon>
+            <span class="layout-text">选项 3</span>
+          </Menu-item>
+        </Menu>
+      </i-col>
+      <i-col :span="spanRight">
+        <div class="layout-header">
+          <i-button type="text" @click="toggleClick">
+            <Icon type="navicon" size="32"></Icon>
+          </i-button>
+          积分银行后台管理
+        </div>
+        <div class="layout-breadcrumb">
+          <Breadcrumb>
+            <Breadcrumb-item href="#">首页</Breadcrumb-item>
+            <Breadcrumb-item href="#">应用中心</Breadcrumb-item>
+            <Breadcrumb-item>某应用</Breadcrumb-item>
+          </Breadcrumb>
+        </div>
+        <div class="layout-content">
+          <div class="layout-content-main">内容区域</div>
+        </div>
+        <div class="layout-copy">
+          2011-2016 &copy; TalkingData
+        </div>
+      </i-col>
+    </Row>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    data () {
+      return {
+        spanLeft: 5,
+        spanRight: 19
+      }
+    },
+    computed: {
+      iconSize () {
+        return this.spanLeft === 5 ? 14 : 24;
+      }
+    },
+    methods: {
+      toggleClick () {
+        if (this.spanLeft === 5) {
+          this.spanLeft = 2;
+          this.spanRight = 22;
+        } else {
+          this.spanLeft = 5;
+          this.spanRight = 19;
+        }
+      }
+    }
+  }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
